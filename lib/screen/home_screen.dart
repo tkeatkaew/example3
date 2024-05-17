a027b39d202619ef681c338a61974b8504941189
Thanit Keatkaew <tkeatkaew@gmail.com>
2024-05-17 10:00:51 +0700

    first commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Example 3'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(children: [
+            Icon(Icons.call),
+            Text("Call"),
+          ]),
+          Column(children: [
+            Icon(Icons.route_rounded),
+            Text("Route"),
+          ]),
+          Column(children: [
+            Icon(Icons.share),
+            Text("Share"),
+          ]),
+        ],
+      ),
+    );
+  }
+}
